42313c9374d4d6a54c5860f9f011a3c1cb5bba6f
Joffrey Jaffeux <j.jaffeux@gmail.com>
2013-01-27 16:34:32 +0100

    refactored sync to be an object instead of a uiviewcontroller, also added block syntax for control flow

---
+++ lib/motionscan/sync.rb
@@ -0,0 +1,47 @@
+module Motionscan
+
+  class Sync
+
+    attr_accessor :scanner
+
+    def self.init
+      instance = allocate
+      instance
+    end
+
+    def startWithStatus(syncStarted, success:syncCompleted, 
+      error:syncFailed, progress:syncProgressed)
+      @syncStarted = syncStarted
+      @syncCompleted = syncCompleted
+      @syncFailed = syncFailed
+      @syncProgressed = syncProgressed
+
+      @scanner = MSScanner.sharedInstance
+      @scanner.syncWithDelegate(self) unless @scanner.isSyncing
+    end
+
+    def scannerWillSync(scanner)
+      @syncStarted.call
+    end
+
+    def didSyncWithProgress(progress, total:total)
+      @syncProgressed.call(progress, total)
+    end
+
+    def scannerDidSync(scanner)
+      @lastSync = NSDate.date.timeIntervalSince1970
+      @syncCompleted.call(scanner.count(nil))
+    end
+    
+    def scanner(scanner, failedToSyncWithError:error)
+      errorCode = error.code
+      if errorCode >= 0 and errorCode != MS_BUSY
+        errorString = NSString.stringWithCString(ms_errmsg(errorCode), encoding:NSUTF8StringEncoding)
+        NSLog("[MOODSTOCKS SDK] FAILED TO SYNC WITH ERROR : #{errorString}.")
+      end
+      @syncFailed.call(error)
+    end
+
+  end
+
+end
