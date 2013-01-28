f3a4ad7502790efe9176dadc8255edfa553c0b8d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2013-01-28 14:13:03 +0100

    Apply renamed scannerView to view in flash.rb

--- lib/motionscan/flash.rb
+++ lib/motionscan/flash.rb
@@ -3,9 +3,9 @@ module Motionscan
   module Flash
 
     def flash
-      flashView = UIView.alloc.initWithFrame(self.scannerView.bounds)
+      flashView = UIView.alloc.initWithFrame(@view.bounds)
       flashView.backgroundColor = UIColor.whiteColor
-      self.scannerView.addSubview(flashView)
+      @view.addSubview(flashView)
 
       UIView.animateWithDuration(0.4,
         animations:lambda { flashView.alpha = 0.0 },
